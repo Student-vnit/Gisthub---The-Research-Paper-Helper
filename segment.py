f7d4cdd6a024b58d73d3183ea009a56d5bdf3112
Abhishek Kumar Yadav <abhk943@gmail.com>
2021-04-09 23:47:24 +0530

    Heroku changed requirements

--- segment.py
+++ segment.py
@@ -8,46 +8,47 @@ class ProbDist(dict):
         data = {}
         with open(datafile) as f:
             for line in f:
-                k, v = line.rstrip().split('\t')
+                k, v = line.rstrip().split("\t")
                 data[k] = int(v)
 
         for k, c in data.items():
             self[k] = self.get(k, 0) + c
 
         if unigram:
-            self.unknownprob = lambda k, N: 10 / \
-                (N*10**len(k))    # avoid unknown long word
+            self.unknownprob = lambda k, N: 10 / (
+                N * 10 ** len(k)
+            )  # avoid unknown long word
         else:
-            self.unknownprob = lambda k, N: 1/N
+            self.unknownprob = lambda k, N: 1 / N
 
         self.N = N
 
     def __call__(self, key):
         if key in self:
-            return self[key]/self.N
+            return self[key] / self.N
         else:
             return self.unknownprob(key, self.N)
 
 
-P_unigram = ProbDist('unigrams.txt')
-P_bigram = ProbDist('bigrams.txt', False)
+P_unigram = ProbDist("unigrams.txt")
+P_bigram = ProbDist("bigrams.txt", False)
 
 
 def conditionalProb(word_curr, word_prev):
     # Conditional probability of current word given the previous word.
     try:
-        return P_bigram[word_prev + ' ' + word_curr]/P_unigram[word_prev]
+        return P_bigram[word_prev + " " + word_curr] / P_unigram[word_prev]
     except KeyError:
         return P_unigram(word_curr)
 
 
-@functools.lru_cache(maxsize=2**10)
-def viterbi(text, prev='<S>', maxlen=20):
+@functools.lru_cache(maxsize=2 ** 10)
+def viterbi(text, prev="<S>", maxlen=20):
     if not text:
         return 0.0, []
 
     textlen = min(len(text), maxlen)
-    splits = [(text[:i + 1], text[i + 1:]) for i in range(textlen)]
+    splits = [(text[: i + 1], text[i + 1 :]) for i in range(textlen)]
 
     candidates = []
     for first_word, remain_word in splits:
@@ -55,10 +56,9 @@ def viterbi(text, prev='<S>', maxlen=20):
         first_prob = math.log10(conditionalProb(first_word, prev))
         remain_prob, remain_word = viterbi(remain_word, first_word)
 
-        candidates.append(
-            (first_prob + remain_prob, [first_word] + remain_word))
+        candidates.append((first_prob + remain_prob, [first_word] + remain_word))
 
     return max(candidates)
 
 
-print(viterbi('Detection'))
+# print(viterbi('Detection'))
--- tests/testapi.py
+++ tests/testapi.py
@@ -2,7 +2,7 @@ import requests
 from io import BytesIO
 
 pdfpath = "/home/abhk943/Documents/vnitSem6/softLabTTS/r1.pdf"
-url = "http://127.0.0.1:8000/pdf_initialize"
+url = "https://gisthub-vnit.herokuapp.com/pdf_initialize"
 pdf = open(pdfpath, "rb")
 files = {"byteFile": pdf, "filename": pdfpath}
 r = requests.post(url, files=files)
