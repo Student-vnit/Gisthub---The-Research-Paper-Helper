1aa1596f0ef9aa564b1b3a7564fc4bf0190f2748
Warun Panpaliya <51282650+warun1801@users.noreply.github.com>
2021-04-11 20:37:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ async def pdfToText(
             f.write(chunk)
 
     # URL for ngrok server running at colab, to serve summary
-    url = "http://078aa0e2017c.ngrok.io"
+    url = "http://6c5e62cc9c4f.ngrok.io"
 
     # background_task for text, audio, summary, runs after return
     background_tasks.add_task(pdf_all, byteFile.filename)
